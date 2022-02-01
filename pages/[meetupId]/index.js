a6bce3eb6cda18eb8cf6106e4f134bd6491bcee9
Dung Viet <vietdung299@gmail.com>
2022-02-01 13:43:18 +0700

    change fallback

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -31,7 +31,7 @@ export async function getStaticPaths() {
   client.close();
 
   return {
-    fallback: false,
+    fallback: "blocking",
     paths: meetups.map((item) => ({
       params: { meetupId: item._id.toString() },
     })),
